3f820aebf67e5cd03eec82307db8df4ee1952fb9
dumbledary <31068790+dumbledary@users.noreply.github.com>
2017-10-13 16:46:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zlongame0229/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.zlongame0229.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.btn_0:
+            case R.id.btn_1:
+            case R.id.btn_2:
+            case R.id.btn_3:
+            case R.id.btn_4:
+            case R.id.btn_5:
+            case R.id.btn_6:
+            case R.id.btn_7:
+            case R.id.btn_8:
+            case R.id.btn_9:
+            case R.id.btn_add:
+            case R.id.btn_sub:
+            case R.id.btn_mul:
+            case R.id.btn_div:
+            case R.id.btn_dot:
+            case R.id.btn_c:
+            case R.id.btn_del:
+            case R.id.btn_equ:
+                break;
+        }
+    }
+}
